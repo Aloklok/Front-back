f1f76da076ae634b1d1f25f9913d07d77a864f28
Alok2333 <alok2333@outlook.com>
2016-11-18 20:42:08 +0800

    v1

---
+++ video-share-socketio/app/main.js
@@ -0,0 +1,66 @@
+var userId = localStorage.getItem('userId') || randomId();
+var messageCache;
+
+localStorage.setItem('userId',userId);
+console.info('Hi i am user #'+ userId);
+
+function randomId(){
+	return Math.floor(Math.random()*1e11);
+};
+
+var socket = io.connect('http://localhost:1000',{'forceNew':true}); 
+//force to create new connection everytime,no-cache
+socket.on('message',function(data){
+	console.info(data);
+	messageCache = data;
+	render();
+});
+
+function render(){
+	var data = messageCache;
+	var html = data.sort(function(a,b){
+		return a.ts - b.ts;
+	}).map(function(data,index){
+		return (`
+		<form class='message' onsubmit='return likeMessage(messageCache[${index}])'>
+			<div class='name'> ${data.userName} </div>
+				<a href='${data.content.link}' class='message' target=_blank>
+					${data.content.text}
+				</a>
+				<div class='time'>${moment(data.ts).fromNow()}</div>	
+				<input type='submit' class='like-count' value="${data.likedBy.length} Likes"/>			
+		</form>		
+				
+			`)
+	}).join('');
+	document.getElementById('messages').innerHTML = html;
+};
+
+//add extra prop : messageId
+function addMessage(e){
+	var payload = {
+		messageId: randomId(),
+		userName:document.getElementById('username').value,
+		content: {
+			text:document.getElementById('message').value,
+			link:document.getElementById('linkAddress').value
+	},
+		likedBy:[],
+		ts:Date.now()
+	}
+	socket.emit('new-message',payload);
+	return false;
+};
+
+//update one message:likedBy 
+function likeMessage(message){
+	var index = message.likedBy.indexOf(userId);
+	if(index<0){
+		message.likedBy.push(userId)
+	} else{
+		message.likedBy.splice(index,1) //cancel the like
+	}
+	socket.emit('update-message',message);
+	render(); //update the messageCache then render again;
+	return false;
+};
---
+++ video-share-socketio/server/main.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server); //automatically create ../socket.io/socket.io.js
+
+
+app.use(express.static('app'));
+app.use('/bower_components',express.static('bower_components'));
+
+
+io.on('connection',function(socket){
+	console.log('connect to socket!');
+	socket.emit('message',messages);
+	socket.on('new-message',function(data){
+		messages.push(data)
+		io.sockets.emit('message',messages); //all the clients
+		}); 
+
+
+	socket.on('update-message',function(data){
+		var message = messages.filter(function(message){
+			return message.messageId == data.messageId
+		})[0];
+		message.likedBy = data.likedBy;
+		io.sockets.emit('message',messages); //all the clients
+		}); 
+});
+
+server.listen(1000);
+
+var messages = [{
+  userId:1,
+  messageId:10,
+  userName:"Asha Greyjoy",
+  content:{
+    text:"The stone tree of the Stonetrees.",
+    link:"http://awoiaf.westeros.org/index.php/House_Stonetree"
+  },
+  likedBy:[1],
+  ts:Date.now() - 10000
+},{
+  userId:2,
+  messageId:11,
+  userName:"Arya Stark",
+  content:{
+    text:"We'll come see this inn.",
+    link:"http://gameofthrones.wikia.com/wiki/Inn_at_the_Crossroads"
+  },
+  likedBy:[2,3],
+  ts:Date.now() - 100000
+},{
+  userId:3,
+  messageId:14,
+  userName:"Cersei Lannister",
+  content:{
+    text:"Her scheming forced this on me.",
+    link:"http://gameofthrones.wikia.com/wiki/Margaery_Tyrell"
+  },
+  likedBy:[],
+  ts:Date.now() - 1000000
+}];
